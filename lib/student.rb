cb07e0345f9ddddd7614d4e4a5c80bdacd411916
Denali Balser <denali.balser167@gmail.com>
2019-09-26 00:08:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,18 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end 
+
+
+  end
 
 
 end
